87de88bd322050a6177f20fc02591597a18b6b01
Tom Elliott <ipse@paregorios.org>
2025-10-16 13:24:47 -0400

    stub place making

---
+++ src/pleiades_accession/making.py
@@ -0,0 +1,43 @@
+#
+# This file is part of pleiades_accession
+# by Tom Elliott for the Institute for the Study of the Ancient World
+# (c) Copyright 2025 by New York University
+# Licensed under the AGPL-3.0; see LICENSE.txt file.
+#
+
+"""
+Make new LPF from scratch, using provided resources
+"""
+class LPFPlace:
+    """
+    Class representing a place after Linked Places Format (LPF)
+    """
+
+    def __init__(self):
+        """
+        Initialize LPFPlace class
+        """
+        pass
+
+    def to_dict(self) -> dict:
+        """
+        Convert LPFPlace to dictionary
+        """
+        return {}
+    
+class Maker:
+    """
+    Class to make new LPF from scratch, using provided resources
+    """
+
+    def __init__(self, sources: list = []) -:
+        """
+        Initialize Maker class
+        """
+        pass
+
+    def make(self):
+        """
+        Make new LPF
+        """
+        pass
